a72e6adf0792956b8ec301d1729b0e82d0416114
KaiNiggemann <kai.niggemann@gmx.de>
2025-05-13 15:09:41 +0200

    Create app.py

---
+++ modules/consumer/app.py
@@ -0,0 +1,12 @@
+import json
+from .services import retrieve_orders, create_order
+
+from kafka import KafkaConsumer
+
+TOPIC_NAME = 'locations'
+
+
+if __name__ == '__main__':
+  consumer = KafkaConsumer(TOPIC_NAME)
+  for message in consumer:
+    print (message)
