57af30bda49e11ce8e6c67cb8d5afa3e05992628
KaiNiggemann <kai.niggemann@gmx.de>
2025-05-14 14:43:14 +0200

    Update app.py

--- modules/consumer/app.py
+++ modules/consumer/app.py
@@ -7,9 +7,9 @@ TOPIC_NAME = 'locations'
 
 
 if __name__ == '__main__':
-  consumer = KafkaConsumer(TOPIC_NAME, value_deserializer=lambda m: json.loads(m.decode('ascii')))
+  consumer = KafkaConsumer(TOPIC_NAME)
   for message in consumer:
     try:
-      print (message.value)
+      print ( json.loads(message.value.decode('ascii')) )
     except:
       print("This seems not a valid JSON format with double quotes")
