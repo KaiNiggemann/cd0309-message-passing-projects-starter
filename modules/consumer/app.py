2de6ff84ca212d64024be04887d90225c4fbf297
KaiNiggemann <kai.niggemann@gmx.de>
2025-05-14 15:26:14 +0200

    Update app.py

--- modules/consumer/app.py
+++ modules/consumer/app.py
@@ -11,23 +11,30 @@ def consumer1():
   for message in consumer:
     try:
       content = json.loads(message.value.decode('ascii'))
-      print ("Location - processing message: " + str(content))
-      Location = LocationService.create(content)
-      print (Location)
     except:
       print("This seems not a valid JSON format with double quotes")
-
+  
+    print ("Location - processing message: " + str(content))
+    Location = LocationService.create(content)
+    print (Location)
+    
+  return
+  
 def consumer2():
   print("Start consuming 'persons'...")
   consumer = KafkaConsumer('persons')
   for message in consumer:
     try:
       content = json.loads(message.value.decode('ascii'))
-      print ("Person - processing message: " + str(content))
-      Person = PersonService.create(payload)
-      print (Person)
     except:
       print("This seems not a valid JSON format with double quotes")
+      
+    print ("Person - processing message: " + str(content))
+    Person = PersonService.create(payload)
+    print (Person)
+
+return
+
 
 
 if __name__ == '__main__':
