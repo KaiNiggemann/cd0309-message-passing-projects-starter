18045ca089aece80b3f27de244d9df77a9dd288a
KaiNiggemann <kai.niggemann@gmx.de>
2025-05-14 15:04:40 +0200

    Update app.py

--- modules/consumer/app.py
+++ modules/consumer/app.py
@@ -1,15 +1,34 @@
 import json
+import threading
 from services import LocationService, PersonService #ConnectionService
 import asyncio
 from kafka import KafkaConsumer
 
-TOPIC_NAME = 'locations'
+
+def consumer1(self):
+  print("Start consuming 'locations'...")
+  consumer = KafkaConsumer('locations')
+  for message in consumer:
+    try:
+      print ("Location:" + str(json.loads(message.value.decode('ascii')) ))
+    except:
+      print("This seems not a valid JSON format with double quotes")
+
+def cons2(self):
+  print("Start consuming 'persons'...")
+  consumer = KafkaConsumer('locations')
+  for message in consumer:
+    try:
+      print ("Person:" + str( json.loads(message.value.decode('ascii')) ))
+    except:
+      print("This seems not a valid JSON format with double quotes")
 
 
 if __name__ == '__main__':
-  consumer = KafkaConsumer(TOPIC_NAME)
-  for message in consumer:
-    try:
-      print ( json.loads(message.value.decode('ascii')) )
-    except:
-      print("This seems not a valid JSON format with double quotes")
+    consumer1_thread = threading.Thread(target=consumer1, args=())
+    consumer2_thread = threading.Thread(target=consumer2, args=())
+    consumer1_thread.daemon = True
+    consumer2_thread.daemon = True
+    consumer1_thread.start()
+    consumer2_thread.start()
+
