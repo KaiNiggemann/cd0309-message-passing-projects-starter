a7a1f666f7f5ffaadfb3ea6a56aec1a177b37b64
KaiNiggemann <kai.niggemann@gmx.de>
2025-05-16 15:36:40 +0200

    Update __init__.py

--- modules/consumer/app/__init__.py
+++ modules/consumer/app/__init__.py
@@ -1,7 +1,9 @@
 from flask import Flask, jsonify
 from flask_cors import CORS
 from flask_restx import Api
+from flask_sqlalchemy import SQLAlchemy
 
+db = SQLAlchemy()
 
 def create_app(env=None):
     from app.config import config_by_name
