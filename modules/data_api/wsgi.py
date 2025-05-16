0c4973561de6f39fe2347af711446d07749e342e
KaiNiggemann <kai.niggemann@gmx.de>
2025-05-16 11:00:02 +0200

    Update wsgi.py

--- modules/data_api/wsgi.py
+++ modules/data_api/wsgi.py
@@ -3,9 +3,8 @@ import os
 from app import create_app
 
 
-app, bus = create_app(os.getenv("FLASK_ENV") or "test")
+app = create_app(os.getenv("FLASK_ENV") or "test")
 
 
 if __name__ == "__main__":
-    bus.run()
     app.run(debug=True)
