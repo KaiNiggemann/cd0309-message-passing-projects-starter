0bad7b8712e34273589b8d67b66be581afb85dd6
KaiNiggemann <kai.niggemann@gmx.de>
2025-05-16 11:10:24 +0200

    Update routes.py

--- modules/data_api/app/routes.py
+++ modules/data_api/app/routes.py
@@ -1,5 +1,5 @@
-def register_routes(api, app, bus, root="api"):
+def register_routes(api, app, root="api"):
     from app.udaconnect import register_routes as attach_udaconnect
 
     # Add routes
-    attach_udaconnect(api, app, bus)
+    attach_udaconnect(api, app)
