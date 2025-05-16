990f67704df5843322bce02242767cb8c276c809
KaiNiggemann <kai.niggemann@gmx.de>
2025-05-16 14:32:11 +0200

    Update app.py

--- modules/consumer/app.py
+++ modules/consumer/app.py
@@ -1,8 +1,10 @@
 import json
 import os
+import threading
 from services import LocationService, PersonService #ConnectionService
 
 from flask_kafka import FlaskKafka
+from kafka import KafkaConsumer
 from app import create_app
 
 
@@ -39,6 +41,41 @@ def test_topic_handler(consumer,msg):
   return
 
 
+def consumer1():
+  print("Start consuming 'locations'...")
+  consumer = KafkaConsumer('locations', bootstrap_servers='kafka-broker.default.svc.cluster.local:9092'))
+  for message in consumer:
+    try:
+      content = json.loads(message.value.decode('ascii'))
+    except:
+      print("This seems not a valid JSON format with double quotes")
+
+    print ("Location - processing message: " + str(content))
+    Location = LocationService.create(content)
+    print (Location)
+
+  return
+
+def consumer2():
+  print("Start consuming 'persons'...")
+  consumer = KafkaConsumer('persons', bootstrap_servers='kafka-broker.default.svc.cluster.local:9092'))
+  for message in consumer:
+    try:
+      content = json.loads(message.value.decode('ascii'))
+    except:
+      print("This seems not a valid JSON format with double quotes")
+
+    print ("Person - processing message: " + str(content))
+    Person = PersonService.create(content)
+    print (Person)
+
+  return
+
+
 if __name__ == '__main__':
-  bus.run()
-  app.run(debug=True)
+    consumer1_thread = threading.Thread(target=consumer1)
+    consumer2_thread = threading.Thread(target=consumer2)
+    consumer1_thread.start()
+    consumer2_thread.start()
+    app.run(debug=True)
+
