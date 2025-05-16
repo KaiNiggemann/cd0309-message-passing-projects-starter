b510c83556b0c1cf157068af8e62e6f8a18ce01f
KaiNiggemann <kai.niggemann@gmx.de>
2025-05-16 17:18:36 +0200

    Update __init__.py

--- modules/consumer/app/__init__.py
+++ modules/consumer/app/__init__.py
@@ -2,8 +2,15 @@ from flask import Flask, jsonify
 from flask_cors import CORS
 from flask_restx import Api
 from flask_sqlalchemy import SQLAlchemy
-
 db = SQLAlchemy()
+
+from app.config import config_by_name
+from sqlalchemy import create_engine
+
+
+print("Connecting to: " + config_by_name[os.getenv("FLASK_ENV") or "test"].SQLALCHEMY_DATABASE_URI)
+engine = create_engine(config_by_name[os.getenv("FLASK_ENV") or "test"].SQLALCHEMY_DATABASE_URI)
+
 
 def create_app(env=None):
     from app.config import config_by_name
