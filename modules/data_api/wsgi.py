76d4e70747fc5dec723011bb2235b5f9d4e3d57e
KaiNiggemann <kai.niggemann@gmx.de>
2025-05-16 09:50:17 +0200

    Update wsgi.py

--- modules/data_api/wsgi.py
+++ modules/data_api/wsgi.py
@@ -1,7 +1,12 @@
 import os
 
+from flask_kafka import FlaskKafka
 from app import create_app
 
+
 app = create_app(os.getenv("FLASK_ENV") or "test")
+bus = FlaskKafka()
+bus.init_app(app)
+
 if __name__ == "__main__":
     app.run(debug=True)
