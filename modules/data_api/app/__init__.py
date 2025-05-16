5e74cf7c83feb0b5707225c4cc6faf2e4e7e3db9
KaiNiggemann <kai.niggemann@gmx.de>
2025-05-16 11:32:17 +0200

    Update __init__.py

--- modules/data_api/app/__init__.py
+++ modules/data_api/app/__init__.py
@@ -2,7 +2,6 @@ from flask import Flask, jsonify
 from flask_cors import CORS
 from flask_restx import Api
 from flask_sqlalchemy import SQLAlchemy
-from flask_kafka import FlaskKafka
 
 db = SQLAlchemy()
 
