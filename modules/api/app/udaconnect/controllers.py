46ba756b9f55d41b2aad92935503b5144d9a557b
KaiNiggemann <kai.niggemann@gmx.de>
2025-05-19 13:58:26 +0200

    Update controllers.py

--- modules/api/app/udaconnect/controllers.py
+++ modules/api/app/udaconnect/controllers.py
@@ -17,25 +17,6 @@ DATE_FORMAT = "%Y-%m-%d"
 api = Namespace("UdaConnect", description="Connections via geolocation.")  # noqa
 
 
-# TODO: This needs better exception handling
-
-
-@api.route("/locations")
-@api.route("/locations/<location_id>")
-@api.param("location_id", "Unique ID for a given Location", _in="query")
-class LocationResource(Resource):
-    @accepts(schema=LocationSchema)
-    @responds(schema=LocationSchema)
-    def post(self) -> Location:
-        request.get_json()
-        location: Location = LocationService.create(request.get_json())
-        return location
-
-    @responds(schema=LocationSchema)
-    def get(self, location_id) -> Location:
-        location: Location = LocationService.retrieve(location_id)
-        return location
-
 
 @api.route("/persons")
 class PersonsResource(Resource):
@@ -50,15 +31,6 @@ class PersonsResource(Resource):
     def get(self) -> List[Person]:
         persons: List[Person] = PersonService.retrieve_all()
         return persons
-
-
-@api.route("/persons/<person_id>")
-@api.param("person_id", "Unique ID for a given Person", _in="query")
-class PersonResource(Resource):
-    @responds(schema=PersonSchema)
-    def get(self, person_id) -> Person:
-        person: Person = PersonService.retrieve(person_id)
-        return person
 
 
 @api.route("/persons/<person_id>/connection")
