067ac7c39daf7739df141d80837512e25b8a68bd
KaiNiggemann <kai.niggemann@gmx.de>
2025-05-19 12:08:41 +0200

    Update show_persons.sh

--- scripts/show_persons.sh
+++ scripts/show_persons.sh
@@ -5,4 +5,4 @@ export CT_DB_USERNAME=ct_admin
 export CT_DB_NAME=geoconnections
 
 
-cat ./db/show_person.sql | kubectl exec -i $1 -- bash -c "psql -U $CT_DB_USERNAME -d $CT_DB_NAME"
+cat ./db/show_persons.sql | kubectl exec -i $1 -- bash -c "psql -U $CT_DB_USERNAME -d $CT_DB_NAME"
