85041057365c65895fa2a1b293020c4f86bb2d26
KaiNiggemann <kai.niggemann@gmx.de>
2025-05-20 09:46:11 +0200

    Update controllers.py

--- modules/api/app/udaconnect/controllers.py
+++ modules/api/app/udaconnect/controllers.py
@@ -12,6 +12,8 @@ from flask_accepts import accepts, responds
 from flask_restx import Namespace, Resource
 from typing import Optional, List
 
+import requests
+
 DATE_FORMAT = "%Y-%m-%d"
 
 api = Namespace("UdaConnect", description="Connections via geolocation.")  # noqa
@@ -21,16 +23,17 @@ api = Namespace("UdaConnect", description="Connections via geolocation.")  # noq
 @api.route("/persons")
 class PersonsResource(Resource):
     @accepts(schema=PersonSchema)
-    @responds(schema=PersonSchema)
-    def post(self) -> Person:
-        payload = request.get_json()
-        new_person: Person = PersonService.create(payload)
-        return new_person
+#    @responds(schema=PersonSchema)
+#    def post(self) -> Person:
+#        payload = request.get_json()
+#        new_person: Person = PersonService.create(payload)
+#        return new_person
 
     @responds(schema=PersonSchema, many=True)
     def get(self) -> List[Person]:
-        persons: List[Person] = PersonService.retrieve_all()
-        return persons
+        persons = requests.get("udaconnect-data-api.default.svc.cluster.local:30002/api/persons")
+        #persons: List[Person] = PersonService.retrieve_all()
+        return persons.text
 
 
 @api.route("/persons/<person_id>/connection")
